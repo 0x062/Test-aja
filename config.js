c65c3e4110fe24d924fc932b669c53155a6be5b3
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-04 18:57:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ export const CONTRACT_ADDRESSES = {
     USDC: '0xb2C1C007421f0Eb5f4B3b3F38723C309Bb208d7d',
     // Ganti dengan alamat Router DEX yang sudah kamu temukan
     ROUTER: '0xdc7D6b58c89A554b3FDC4B5B10De9b4DbF39FB40',
+    WXOS: '0x0AAB67cf6F2e99847b9A95DeC950B250D648c1BB'
 };
 
 // Di sini kita kumpulkan semua ABI (Application Binary Interface).
@@ -20,6 +21,6 @@ export const CONTRACT_ABIS = {
     ],
     // Nanti kita akan tambahkan ABI untuk Router di sini saat masuk Babak 4
     ROUTER: [
-        // ... akan diisi nanti
+        "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable"
     ]
 };
